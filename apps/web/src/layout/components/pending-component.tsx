56abe4bada50dc7d8ac51e5deda6377ff7a302ee
vasyaqwe <elekton.fafik@gmail.com>
2025-06-06 18:59:59 +0300

    improve pending component

--- apps/web/src/layout/components/pending-component.tsx
+++ apps/web/src/layout/components/pending-component.tsx
@@ -1,8 +1,11 @@
 import { Logo } from "@unfiddle/ui/components/logo"
 
-export function PendingComponent() {
+export function PendingComponent({ animated = false }: { animated?: boolean }) {
    return (
-      <main className="motion-preset-slide-up motion-delay-300 motion-translate-y-in-50 motion-preset-blur-up motion-ease-spring-bouncier absolute inset-0 m-auto flex size-fit items-center gap-2.5">
+      <main
+         data-animated={animated ? "" : undefined}
+         className="data-animated:motion-preset-slide-up motion-delay-300 data-animated:motion-translate-y-in-50 data-animated:motion-preset-blur-up md:motion-ease-spring-bouncier absolute inset-0 m-auto flex size-fit items-center gap-2.5"
+      >
          <Logo className="size-6" />
          <p
             className="font-medium text-foreground/75 text-lg"
--- apps/web/src/routes/_authed.tsx
+++ apps/web/src/routes/_authed.tsx
@@ -1,5 +1,6 @@
 import { CACHE_FOREVER } from "@/api"
 import { Main } from "@/layout/components/main"
+import { PendingComponent } from "@/layout/components/pending-component"
 import { Sidebar } from "@/layout/components/sidebar"
 import { trpc } from "@/trpc"
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
@@ -27,7 +28,9 @@ export const Route = createFileRoute("/_authed")({
    pendingComponent: () => (
       <>
          <Sidebar />
-         <Main className="h-screen" />
+         <Main className="h-screen">
+            <PendingComponent animated />
+         </Main>
       </>
    ),
 })
