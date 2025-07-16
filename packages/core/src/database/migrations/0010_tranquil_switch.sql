321fc4f17ed9ed4184fd3cdf26cd740d5eee80ef
vasyaqwe <elekton.fafik@gmail.com>
2025-07-16 22:42:14 +0300

    deploy

---
+++ packages/core/src/database/migrations/0010_tranquil_switch.sql
@@ -0,0 +1,9 @@
+-- This is a 100% harmless SQL migration file for Drizzle ORM.
+-- It uses empty DO blocks to satisfy the "cannot be empty" requirement
+-- without performing any actual database changes.
+
+-- Up migration
+DO $$ BEGIN END; $$ LANGUAGE plpgsql;
+
+-- Down migration
+DO $$ BEGIN END; $$ LANGUAGE plpgsql;
