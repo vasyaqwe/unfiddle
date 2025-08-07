4079ea2357b3e690c87372d6c10e8c29e70d87e1
vasyaqwe <elekton.fafik@gmail.com>
2025-08-07 19:19:16 +0300

    add collaboration worker

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,3 @@
 import { routes } from "@unfiddle/core/api"
-export { Order } from "@unfiddle/core/order/party"
-export { Procurement } from "@unfiddle/core/procurement/party"
-export { Whiteboard } from "@unfiddle/core/whiteboard/party"
 
 export default routes
