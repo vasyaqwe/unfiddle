4ff70271d93a422cadff47102a86842eb0c7bc21
vasyaqwe <elekton.fafik@gmail.com>
2025-08-07 16:46:46 +0300

    add DO

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,19 @@
 import { routes } from "@unfiddle/core/api"
 
+export class Partykit {
+   private state: DurableObjectState
+   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+   private env: any
+
+   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+   constructor(state: DurableObjectState, env: any) {
+      this.state = state
+      this.env = env
+   }
+
+   async fetch(_request: Request): Promise<Response> {
+      return new Response("Hello from the Durable Object!")
+   }
+}
+
 export default routes
