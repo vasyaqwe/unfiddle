e6615e7f21440680a6f4b86fd5512e9e1f2404e8
vasyaqwe <elekton.fafik@gmail.com>
2025-11-17 14:10:46 +0200

    comment out useAppSocket

--- apps/web/src/socket/provider.tsx
+++ apps/web/src/socket/provider.tsx
@@ -3,18 +3,17 @@ import { useEstimateSocket } from "@/estimate/socket"
 import { useOrderSocket } from "@/order/socket"
 import { useProcurementSocket } from "@/procurement/socket"
 import { SocketContext } from "@/socket"
-import { useAppSocket } from "@/socket/app"
 
 export function SocketProvider({ children }: { children: React.ReactNode }) {
    const order = useOrderSocket()
    const procurement = useProcurementSocket()
    const estimate = useEstimateSocket()
    const estimateProcurement = useEstimateProcurementSocket()
-   const app = useAppSocket()
+   // const app = useAppSocket()
 
    return (
       <SocketContext
-         value={{ order, procurement, app, estimate, estimateProcurement }}
+         value={{ order, procurement, estimate, estimateProcurement }}
       >
          {children}
       </SocketContext>
